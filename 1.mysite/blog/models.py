43754f612af96359c58eb5ba461848a70ced9ce5
M. Azmi Sudrajat <44989386+geznerd@users.noreply.github.com>
2022-10-20 11:33:28 +0700

    first commit

---
+++ 1.mysite/blog/models.py
@@ -0,0 +1,36 @@
+from email.policy import default
+from enum import auto
+from select import select
+from statistics import mode
+from turtle import title
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+class Post(models.Model):
+
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
+    title = models.CharField(max_length = 250)
+    slug = models.SlugField(max_length = 250)
+    body = models.TextField()
+    publish = models.DateTimeField(default = timezone.now)
+    created = models.DateTimeField(auto_now_add = True)
+    updated = models.DateTimeField(auto_now = True)
+    status = models.CharField(
+        max_length = 2,
+        choices = Status.choices,
+        default = Status.DRAFT
+    )
+
+    class Meta:
+        ordering = ['-publish']
+        indexes = [
+            models.Index(fields=['-publish']),
+        ]
+
+    def __str__(self):
+        return select.title
